74014caea0a04439ce8534dc1cb114da6ee1ddce
umbreth <umbreth@yahoo.com>
2015-06-27 02:02:21 +0300

    Open main webpage in default browser at start

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-var 	http = require('http'),
-	url = require('url'),
-	fs = require('fs'),
-    	date = require('./js/DatePlain');
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    date = require('./js/DatePlain'),
+    open = require('./js/Open');
 
-var 	start = date.getDate().rest.toString(), // дата и время запуска сервера
-    	ip = '127.0.0.1',
-	port = 1337;
+var start = date.getDate().rest.toString(), // дата и время запуска сервера
+    ip = '127.0.0.1',
+    port = 1337;
 
 // относительные пути к файлам
-var 	path = './',
-	css = './css/global.css',
-	index = './html',
-    	images = './html/images/',
-    	fonts = './fonts/'
-	fourtyfour = read('./html/404.html').content,
-	hashes = ['md5', 'sha1', 'sha256', 'sha224'];
+var path = './',
+    css = './css/global.css',
+    index = './html',
+    images = './html/images/',
+    fonts = './fonts/'
+    fourtyfour = read('./html/404.html').content,
+    hashes = ['md5', 'sha1', 'sha256', 'sha224'];
 
 var t = { // типы контента отдельно
-    	plain: {'content-type': 'text/plain'},
-    	html: {'content-type': 'text/html'},
-    	css: {'content-type': 'text/css'},
-    	js: {'content-type': 'text/javascript'},
-    	svg: {'content-type': 'image/svg+xml'}
+    plain: {'content-type': 'text/plain'},
+    html: {'content-type': 'text/html'},
+    css: {'content-type': 'text/css'},
+    js: {'content-type': 'text/javascript'},
+    svg: {'content-type': 'image/svg+xml'}
 };
 
 function in_array(what, where) { // проверяет, есть ли значение в массиве
@@ -45,20 +46,20 @@ function random(min, max, type) {
 // читает любой файл, возвращает ассоциативный массив
 // с расширением и содержимым. Доступ через точку
 function read(f) {
-	var file = {
-		type: 'text/' + f.substring(f.lastIndexOf('.') + 1),
-		content: fs.readFileSync(f, 'utf-8')
-	};
-	return (file);
+    var file = {
+        type: 'text/' + f.substring(f.lastIndexOf('.') + 1),
+        content: fs.readFileSync(f, 'utf-8')
+    };
+    return (file);
 }
 
 (function() { // основная функция. Оставить одну пару if/else на каждый тип запроса. Остальное нахуй.
-	http.createServer(function(req, res) { // объект сервера с req/res
-		var request = { // слушаем реквесты, записываем в массив
-			path: url.parse(req.url).pathname, // путь
-			query: url.parse(req.url).query // аргумент
-		};
-		if (request.query == 'async') { // отделяю асинхронные запросы от обычных
+    http.createServer(function(req, res) { // объект сервера с req/res
+        var request = { // слушаем реквесты, записываем в массив
+            path: url.parse(req.url).pathname, // путь
+            query: url.parse(req.url).query // аргумент
+        };
+        if (request.query == 'async') { // отделяю асинхронные запросы от обычных
             var async = {
                 uptime: {type: t.plain, response: start},
                 random: {type: t.plain, response: Math.random().toString()},
@@ -78,7 +79,7 @@ function read(f) {
                 res.writeHead(404, t.html);
                 res.end(fourtyfour);
             }
-		} else if (in_array(request.path.slice(1), hashes)) { // api для подсчёта хеш-сумм
+        } else if (in_array(request.path.slice(1), hashes)) { // api для подсчёта хеш-сумм
             for (var n=0; n < hashes.length; n++) { // надо бы указать типы явно, без ссылок на массив
                 if (request.path.indexOf(hashes[n]) != -1) {
                     res.writeHead(200, {'content-type': 'text/plain'});
@@ -111,23 +112,23 @@ function read(f) {
                 res.writeHead(200, t.js);
                 res.end(data);
             });
-		} else if (contains(request.path, 'img')) { // жопой чую, что-то тут не так
+        } else if (contains(request.path, 'img')) { // жопой чую, что-то тут не так
             fs.readFile((path + request.path), function(err, data) {
                 if (err) console.log(err);
                 request.path.indexOf('svg') != -1 ? res.writeHead(200, t.svg) : {};
                 res.end(data);
             });  
-		} else if (contains(request.path, 'font')) {
+        } else if (contains(request.path, 'font')) {
             fs.readFile((path + request.path), function(err, data) {
                 if (err) console.log(err);
                 res.end(data);
             });
         } else if (contains(request.path, 'favicon')) { // показываем фавиконку хрому
-			fs.readFile((index + request.path), function(err, data) {
-				if (err) console.log(err);
-				res.end(data);
-			});
-		} else if (contains(request.path, 'randpic')) { // на запрос /randpic отвечает случайной картинкой из папки
+            fs.readFile((index + request.path), function(err, data) {
+                if (err) console.log(err);
+                res.end(data);
+            });
+        } else if (contains(request.path, 'randpic')) { // на запрос /randpic отвечает случайной картинкой из папки
             var arr = fs.readdirSync(images),
                 pic = arr[random(0, arr.length, 'int')];
             fs.readFile(images + pic, function(err, data) {
@@ -135,20 +136,21 @@ function read(f) {
                 res.end(data);
             });
         } else { // дефолтный ответ сервера
-			var r;
-			r = request.path == '/' ? '/main' : request.path;
-			fs.readFile((index + r + '.html'), function(err, html) { // формируем относительный путь к файлу и шарим
-				if (err) { // если страница не найдена, отдаём 404
-					res.writeHead(404, t.html);
-					res.end(fourtyfour);
-				}
-				res.writeHead(200, t.html); // бла-бла-бла
-				res.end(html); // вуаля, отдаём страницу, пляшем
-			});
-		}
-		console.log('запрошена страница ' + request.path + ' с параметром ' + request.query); // брейкпоинт. Потом снесу
-	}).listen(port);
+            var r;
+            r = request.path == '/' ? '/main' : request.path;
+            fs.readFile((index + r + '.html'), function(err, html) { // формируем относительный путь к файлу и шарим
+                if (err) { // если страница не найдена, отдаём 404
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
+                res.writeHead(200, t.html); // бла-бла-бла
+                res.end(html); // вуаля, отдаём страницу, пляшем
+            });
+        }
+        console.log('запрошена страница ' + request.path + ' с параметром ' + request.query); // брейкпоинт. Потом снесу
+    }).listen(port);
 }).call(this);
 
 console.log('Сервер запущен ' + date.getDate().date + ' в ' + date.getDate().time);
 console.log('Порт ' + port + ' доступен');
+open('http://' + ip.toString() + ':' + port.toString());
