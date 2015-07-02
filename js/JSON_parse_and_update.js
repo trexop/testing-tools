51ebf2ae169bb4cc2169fd413232e0a74f5ef30d
umbreth <umbreth@yahoo.com>
2015-07-03 00:09:14 +0300

    Cleanup

--- js/JSON_parse_and_update.js
+++ js/JSON_parse_and_update.js
@@ -2,3 +2,25 @@ exports.config_read = function(){
 	var config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 	return(config);
 };
+
+
+in_array(request.path, config.requests_allowed.path) ? function() {
+    fs.readFile((path + request.path), function(err, data) {
+        if (err) {
+            res.writeHead(404, t.html);
+            res.end(fourtyfour);
+        } 
+        res.writeHead(200, "t." + request.path);
+    }
+} : function() {
+    var r;
+    r = request.path == '/' ? '/main' : request.path;
+    fs.readFile((index + r + '.html'), function(err, html) {
+        if (err) {
+            res.writeHead(404, t.html);
+            res.end(fourtyfour);
+        }
+        res.writeHead(200, t.html);
+        res.end(html);
+    });
+};
--- main.js
+++ main.js
@@ -19,15 +19,15 @@ var path = './',
     index = './html',
     images = './html/images/',
     fonts = './fonts/'
-    fourtyfour = read('./html/404.html').content,
-    hashes = ['md5', 'sha1', 'sha256', 'sha224'];
+    fourtyfour = read('./html/404.html').content;
 
 var t = { // типы контента отдельно
     plain: {'content-type': 'text/plain'},
     html: {'content-type': 'text/html'},
     css: {'content-type': 'text/css'},
     js: {'content-type': 'text/javascript'},
-    svg: {'content-type': 'image/svg+xml'}
+    svg: {'content-type': 'image/svg+xml'},
+    assets: {'content-type': 'text/javascript'}
 };
 
 function in_array(what, where) { // проверяет, есть ли значение в массиве
@@ -58,31 +58,26 @@ function read(f) {
     return (file);
 }
 
-var myPublicIp = function(){
+function getMyPublicIp(){
     http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
         resp.on('data', function(ip) {
-            return(ip.toString());
+            return(JSON.parse(ip));
         });
     });
 };
 
-(function() { // основная функция. Оставить одну пару if/else на каждый тип запроса. Остальное нахуй.
+(function() {
     http.createServer(function(req, res) { // объект сервера с req/res
         var request = { // слушаем реквесты, записываем в массив
             path: url.parse(req.url).pathname, // путь
             query: url.parse(req.url).query, // аргумент
             method: req.method
         };
-        '''
-        if(request.method == "POST"){
-            Заготовка для обработчика POST
-        }
-        '''
         if (request.query == 'async') { // отделяю асинхронные запросы от обычных
             var async = {
                 uptime: {type: t.plain, response: start},
                 random: {type: t.plain, response: Math.random().toString()},
-                publicIp: {type: t.plain, response: myPublicIp},
+                //publicIp: {type: t.plain, response: getMyPublicIp()},
                 css: {type: t.css, response: read(css).content}
             };
             var arr = Object.keys(async),
@@ -99,53 +94,56 @@ var myPublicIp = function(){
                 res.writeHead(404, t.html);
                 res.end(fourtyfour);
             }
-        } else if (in_array(request.path.slice(1), hashes)) { // api для подсчёта хеш-сумм
-            for (var n=0; n < hashes.length; n++) { // надо бы указать типы явно, без ссылок на массив
-                if (request.path.indexOf(hashes[n]) != -1) {
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    if (n === 0) {
-                        res.end(hashcount.md5(request.query));
-                    } else if ( n == 1 ) {
-                        res.end(hashcount.sha1(request.query));
-                    } else if ( n == 2 ) {
-                        res.end(hashcount.sha256(request.query));
-                    } else if ( n == 3 ) {
-                        res.end(hashcount.sha224(request.query));
-                    }
-                }
-            }
         } else if (contains(request.path, 'css')) { // отдельный вызов для стилей
             fs.readFile((path + request.path), function(err, data) {
-                if (err) console.log(err);
-                res.writeHead(200, t.css);
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
+                res.writeHead(200, "t." + request.path);
                 res.end(data);
             });
         } else if (contains(request.path, 'js')) { // отдельный вызов для клиентских скриптов
             fs.readFile((path + request.path), function(err, data) {
-                if (err) console.log(err);
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
                 res.writeHead(200, t.js);
                 res.end(data);
             });
         } else if (contains(request.path, 'assets')) { // отдельный вызов для хуй пойми чего
             fs.readFile((path + request.path), function(err, data) {
-                if (err) console.log(err);
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
                 res.writeHead(200, t.js);
                 res.end(data);
             });
         } else if (contains(request.path, 'img')) { // жопой чую, что-то тут не так
             fs.readFile((path + request.path), function(err, data) {
-                if (err) console.log(err);
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
                 request.path.indexOf('svg') != -1 ? res.writeHead(200, t.svg) : {};
                 res.end(data);
             });  
         } else if (contains(request.path, 'font')) {
             fs.readFile((path + request.path), function(err, data) {
-                if (err) console.log(err);
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
                 res.end(data);
             });
         } else if (contains(request.path, 'favicon')) { // показываем фавиконку хрому
-            fs.readFile((index + request.path), function(err, data) {
-                if (err) console.log(err);
+            fs.readFile((path + request.path), function(err, data) {
+                if (err) {
+                    res.writeHead(404, t.html);
+                    res.end(fourtyfour);
+                }
                 res.end(data);
             });
         } else if (contains(request.path, 'randpic')) { // на запрос /randpic отвечает случайной картинкой из папки
@@ -158,19 +156,19 @@ var myPublicIp = function(){
         } else { // дефолтный ответ сервера
             var r;
             r = request.path == '/' ? '/main' : request.path;
-            fs.readFile((index + r + '.html'), function(err, html) { // формируем относительный путь к файлу и шарим
-                if (err) { // если страница не найдена, отдаём 404
+            fs.readFile((index + r + '.html'), function(err, html) {
+                if (err) {
                     res.writeHead(404, t.html);
                     res.end(fourtyfour);
                 }
-                res.writeHead(200, t.html); // бла-бла-бла
-                res.end(html); // вуаля, отдаём страницу, пляшем
+                res.writeHead(200, t.html);
+                res.end(html);
             });
         }
-        console.log('запрошена страница ' + request.path + ' с параметром ' + request.query); // брейкпоинт. Потом снесу
+        console.log('запрошена страница ' + request.path + ' с параметром ' + request.query);
     }).listen(port);
 }).call(this);
 
+config.auto_main_page_open == true ? open('http://' + host.toString() + ':' + port.toString()) : {};
 console.log('Сервер запущен ' + date.getDate().date + ' в ' + date.getDate().time);
 console.log('Порт ' + port + ' доступен');
-open('http://' + host.toString() + ':' + port.toString());
